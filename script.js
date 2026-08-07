69dd785f1f01be8eeb073b241f43873ae6c60bfd
7daniely2B <daniely.jose@escola.pr.gov.br>
2026-08-07 13:12:42 -0300

    9

---
+++ script.js
@@ -0,0 +1,41 @@
+let tamanhoFonte = 100;
+
+const botaoAumentarFonte = document.getElementById("botao-aumentar-fonte");
+const botaoDiminuirFonte = document.getElementById("botao-diminuir-fonte");
+const botaoLeituraVoz = document.getElementById("botao-leitura-voz");
+
+botaoAumentarFonte.addEventListener("click", () => {
+    if (tamanhoFonte < 200) {
+        tamanhoFonte += 10;
+        document.body.style.fontSize = tamanhoFonte + "%";
+    }
+});
+
+botaoDiminuirFonte.addEventListener("click", () => {
+    if (tamanhoFonte > 70) {
+        tamanhoFonte -= 10;
+        document.body.style.fontSize = tamanhoFonte + "%";
+    }
+});
+
+botaoLeituraVoz.addEventListener("click", () => {
+
+    if ("speechSynthesis" in window) {
+
+        speechSynthesis.cancel();
+
+        const texto = document.body.innerText;
+
+        const fala = new SpeechSynthesisUtterance(texto);
+
+        fala.lang = "pt-BR";
+        fala.rate = 1;
+        fala.pitch = 1;
+
+        speechSynthesis.speak(fala);
+
+    } else {
+        alert("Seu navegador não suporta leitura em voz alta.");
+    }
+
+});
